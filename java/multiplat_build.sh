88b7d56123f0e50f05c8deef3b0a025fd144f106
Babbaj <babbaj45@gmail.com>
2023-07-11 14:18:41 -0400

    modern arch

--- java/multiplat_build.sh
+++ java/multiplat_build.sh
@@ -10,7 +10,7 @@ function do_build {
     local output_path=$3
     echo "Building $target..."
 
-    CXXFLAGS="-target $target" cmake -G Ninja $java_root/.. \
+    CXXFLAGS="-target $target -march=znver3" cmake -G Ninja $java_root/.. \
       -DCMAKE_C_COMPILER=$(realpath $java_root/zigcc.sh) -DCMAKE_CXX_COMPILER=$(realpath $java_root/zigcxx.sh) \
       -DSHARED_LIBRARY=True -DCMAKE_BUILD_TYPE=Release
 
