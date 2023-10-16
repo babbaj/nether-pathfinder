4d84abc843f52622376b51a6cf6056d0ee0909a6
Babbaj <babbaj45@gmail.com>
2023-10-15 22:40:58 -0400

    fix flake

--- flake.nix
+++ flake.nix
@@ -3,32 +3,32 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils/master";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }: let
-    system = "x86_64-darwin";
-    pkgs = import nixpkgs { inherit system; };
-  in {
+  outputs = { self, nixpkgs, flake-utils }:
+      flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            binutils-unwrapped-all-targets
+            clang_16
+            pkg-config
+            cmake
+            ninja
+            zig
+          ];
+          buildInputs = with pkgs; [
+            gbenchmark
+          ];
 
-    devShells.${system}.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [
-        binutils-unwrapped-all-targets
-        clang_16
-        pkg-config
-        cmake
-        ninja
-        zig
-      ];
-      buildInputs = with pkgs; [
-        gbenchmark
-      ];
-
-      shellHook = ''
-        export NIX_CFLAGS_COMPILE="-march=native"
-        export CC=${pkgs.clang_16}/bin/clang
-        export CXX=${pkgs.clang_16}/bin/clang++
-      '';
-    };
-  };
+          shellHook = ''
+            export NIX_CFLAGS_COMPILE="-march=native"
+            export CC=${pkgs.clang_16}/bin/clang
+            export CXX=${pkgs.clang_16}/bin/clang++
+          '';
+        };
+      });
 }
